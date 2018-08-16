e3276cfebeb57dfdb7f808d26ee3c3f14e1148d8
HardenFire <879563765@163.com>
2018-08-17 02:40:36 +0800

    test

--- src/test/java/org/naivechain/block/BlockServiceTest.java
+++ src/test/java/org/naivechain/block/BlockServiceTest.java
@@ -5,5 +5,8 @@ package org.naivechain.block;
  * Created by hongsen on 2017/7/6.
  */
 public class BlockServiceTest {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 
 }
